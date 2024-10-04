4c9d8f9a5ad35f31829ee7f7409a86e043672e66
itsTanishq01 <136373698+itsTanishq01@users.noreply.github.com>
2024-10-05 00:41:57 +0530

    Delete src/TextFeatures.h

--- src/TextFeatures.h
+++
@@ -1,9 +0,0 @@
-#ifndef FEATURES_H
-#define FEATURES_H
-
-#include <string>
-
-// Function to display the search and replace dialog
-void ShowSearchReplaceDialog(bool* open, std::string& searchText, std::string& replaceText, std::string& text);
-
-#endif // FEATURES_H
