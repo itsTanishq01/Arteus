61d8da184e5b105c5c5813cf556e70bb9aeb04d0
itsTanishq01 <136373698+itsTanishq01@users.noreply.github.com>
2024-10-05 00:29:56 +0530

    Update TextFeatures.h

--- src/TextFeatures.h
+++ src/TextFeatures.h
@@ -6,6 +6,4 @@
 // Function to display the search and replace dialog
 void ShowSearchReplaceDialog(bool* open, std::string& searchText, std::string& replaceText, std::string& text);
 
-
-// Function to toggle line wrapping
 #endif // FEATURES_H
